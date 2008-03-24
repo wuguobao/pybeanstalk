fdd4f7d1cf75adc9c0330c7ca1cb62aeed16e7cf
sophacles <sophacles@a7006459-4741-0410-b6e5-73001eb1c069>
2008-03-24 00:46:21 +0000

    Creating tag for 0.9.0 release

---
+++ tags/0.9.0/beanstalk/serverconn.py
@@ -0,0 +1,124 @@
+import socket, select
+import protohandler
+
+_debug = 1
+
+class ServerConn(object):
+    '''ServerConn is a simple, single thread single connection serialized
+    beanstalk connection.  This class is meant to be used as is, or be the base
+    class for more sophisticated connection handling.The methods that are
+    intended to be overridden are the ones that begin with _ and __. These
+    are the meat of the connection handling. The rest are just convenience
+    wrappers around the protohandler methods.
+
+    The Proto class returns a function as part of it's handling/conversion of
+    the beanstalk protocol. This function is threadsafe and event safe, meant
+    to be used as a callback. This should greatly simplify the writing of a
+    twisted or libevent serverconn class'''
+
+    def __init__(self, server, port, job = False):
+        self.server = server
+        self.port = port
+        self.job = job or (lambda **x: x)
+        self.poller = select.poll()
+        self.__makeConn()
+
+    def fileno(self):
+        return self._socket.fileno()
+
+    def __makeConn(self):
+        self._socket = socket.socket()
+        self._socket.connect((self.server, self.port))
+        self.poller.register(self._socket, select.POLLIN)
+
+    def __writeline(self, line):
+        try:
+            self._socket.sendall(line)
+        except:
+            raise protohandler.ProtoError
+
+    def _get_response(self, handler):
+        data = ''
+        while True:
+            if not self.poller.poll(2): continue
+
+            recv = self._socket.recv(protohandler.data_remaining(handler))
+            if not recv:
+                self._socket.close()
+                raise errors.ProtoError("Remote host closed conn")
+            res = handler.send(recv)
+            if res: break
+        return res
+
+
+    def _do_interaction(self, line, handler):
+        self.__writeline(line)
+        return self._get_response(handler)
+
+    @property
+    def tube(self):
+        return self.list_tube_used()['tube']
+
+    def _get_watchlist(self):
+        return self.list_tubes_watched()['data']
+    def _set_watchlist(self, seq):
+        if len(seq) == 0:
+            seq.append('default')
+        seq = set(seq)
+        current = set(self._get_watchlist())
+        add = seq - current
+        rem = current - seq
+
+        for x in add:
+            self.watch(x)
+        for x in rem:
+            self.ignore(x)
+        return
+    watchlist = property(_get_watchlist, _set_watchlist)
+
+    def __getattr__(self, attr):
+        def caller(*args, **kw):
+            return self._do_interaction(\
+                *getattr(protohandler, 'process_%s' % attr)(*args, **kw))
+        return caller
+
+class ThreadedConn(ServerConn):
+    def __init__(self, *args, **kw):
+        if 'pool' in kw:
+            self.__pool = kw.pop('pool')
+        super(ThreadedConn, self).__init__(*args, **kw)
+
+    def __del__(self):
+        self.__pool.release(self)
+        super(ThreadedConn, self).__del__()
+
+class ThreadedConnPool(object):
+    '''
+    ThreadedConnPool: A simple pool class for connection objects).
+    This object will create a pool of size nconns. It does no thread wrangling,
+    and no form of connection management, other than to get a unique connection
+    to the thread that calls get.  In fact this could probably be simplified
+    even more by subclassing Semaphore.
+    '''
+    def __init__(self, nconns, server, port, job = False):
+        self.__conns = list()
+        self.__lock = threading.Lock()
+        if threaded: conntype = ThreadedConn
+        else: conntype = ServerConn
+        for a in range(nconns):
+            self.conns.append(conntype(server, port, job=job, pool=self))
+
+        self.useme = threading.Semaphore(nconns)
+
+    def get(self):
+        self.useme.aquire()
+        self.lock.acquire()
+        ret = self.conns.pop(0)
+        self.lock.release()
+
+    def release(self, conn):
+        self.lock.acquire()
+        self.conns.append(conn)
+        self.lock.release()
+        self.useme.release()
+
---
+++ tags/0.9.0/tests/test_Proto.py
@@ -0,0 +1,175 @@
+import sys
+print >>sys.stderr,sys.version
+sys.path.append('..')
+from nose import with_setup, tools
+from beanstalk import protohandler
+from beanstalk import errors
+
+
+prototest_info = [
+    [
+        ('process_put', ('test_data', 0, 0, 10)),
+        "put 0 0 10 %s\r\ntest_data\r\n" % (len('test_data'),),
+        [
+            ('INSERTED 3\r\n', {'state':'ok','jid':3}),
+            ('BURIED 3\r\n', {'state':'buried','jid':3})
+        ]
+    ],
+    [
+        ('process_use', ('bar',)),
+        'use bar\r\n',
+        [
+            ('USING bar\r\n', {'state':'ok', 'tube':'bar'})
+        ]
+    ],
+    [
+        ('process_reserve', ()),
+        'reserve\r\n',
+        [
+            ('RESERVED 12 5\r\nabcde\r\n',{'state':'ok', 'bytes': 5, 'jid':12,
+                'data':'abcde'})
+        ]
+    ],
+    [
+        ('process_delete', (12,)),
+        'delete 12\r\n',
+        [
+            ('DELETED\r\n',{'state':'ok'})
+        ]
+    ],
+    [
+        ('process_release', (33,22,17)),
+        'release 33 22 17\r\n',
+        [
+            ('RELEASED\r\n',{'state':'ok'}),
+            ('BURIED\r\n',{'state':'buried'})
+        ]
+    ],
+    [
+        ('process_bury', (29, 21)),
+        'bury 29 21\r\n',
+        [
+            ('BURIED\r\n',{'state':'ok'})
+        ]
+    ],
+    [
+        ('process_watch', ('supertube',)),
+        'watch supertube\r\n',
+        [
+            ('WATCHING 5\r\n',{'state':'ok','count': 5})
+        ]
+    ],
+    [
+        ('process_ignore', ('supertube',)),
+        'ignore supertube\r\n',
+        [
+            ('WATCHING 3\r\n', {'state':'ok', 'count':3})
+            #('NOT_IGNORED',{'state':'buried'})
+        ]
+    ],
+    [
+        ('process_peek', (39,)),
+        'peek 39\r\n',
+        [
+            ("FOUND 39 10\r\nabcdefghij\r\n", {'state':'ok', 'jid':39,
+                'bytes':10, 'data':'abcdefghij'})
+        ]
+    ],
+    [
+        ('process_peek', ()),
+        'peek\r\n',
+        [
+            ("FOUND 9 10\r\nabcdefghij\r\n",{'state':'ok', 'jid':9, 'bytes':10,
+                'data':'abcdefghij'})
+        ]
+    ],
+    [
+        ('process_kick', (200,)),
+        'kick 200\r\n',
+        [
+            ("KICKED 59\r\n",{'state':'ok', 'count':59})
+        ]
+    ],
+    [
+        ('process_stats', ()),
+        'stats\r\n',
+        [
+            ('OK 15\r\n---\ntest: good\n\r\n', {'state':'ok', 'bytes':15,
+                'data':{'test':'good'}})
+        ]
+    ],
+    [
+        ('process_stats_tube', ('barbaz',)),
+        'stats-tube barbaz\r\n',
+        [
+            ('OK 15\r\n---\ntest: good\n\r\n',{'state':'ok', 'bytes':15,
+                            'data':{'test':'good'}})
+
+        ]
+    ],
+    [
+        ('process_stats_job', (19,)),
+        'stats-job 19\r\n',
+        [
+            ('OK 15\r\n---\ntest: good\n\r\n',{'state':'ok', 'bytes':15,
+                'data':{'test':'good'}})
+
+        ]
+    ],
+    [
+        ('process_list_tubes', ()),
+        'list-tubes\r\n',
+        [
+            ('OK 20\r\n---\n- default\n- foo\n\r\n', {'state':'ok', 'bytes':20,
+                'data':['default','foo']})
+        ]
+    ],
+    [
+        ('process_list_tube_used',()),
+        'list-tube-used\r\n',
+        [
+            ('USING bar\r\n', {'state':'ok', 'tube':'bar'})
+        ]
+    ],
+    [
+        ('process_list_tubes_watched', ()),
+        'list-tubes-watched\r\n',
+        [
+            ('OK 20\r\n---\n- default\n- foo\n\r\n',{'state':'ok', 'bytes':20,
+                'data':['default','foo']})
+
+        ]
+    ]
+]
+
+
+
+
+def check_line(l1, l2):
+    assert l1 == l2
+
+def check_handler(handlerfunc, response, cv):
+    l = 0
+    while True:
+        l = protohandler.data_remaining(handlerfunc)
+        y = handlerfunc.send(response[:l])
+        response = response[l:]
+        if y:
+            assert y == cv
+            break
+    return
+
+def test_interactions():
+    for test in prototest_info:
+        callinfo, commandline, responseinfo = test
+        func = getattr(protohandler, callinfo[0])
+        args = callinfo[1]
+        for response, resultcomp in responseinfo:
+            line, handler = func(*args)
+            yield check_line, line, commandline
+            yield check_handler, handler, response, resultcomp
+
+def test_put_extra():
+    #check that the put raises the right error on big jobs...
+    tools.assert_raises(errors.JobTooBig, protohandler.process_put,'a' * (2**16),0,0,0)
+
